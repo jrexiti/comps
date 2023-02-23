648ee73e5bf965d551d90216a059512f3ce19bef
Jay Rexiti <jrexiti@totalwine.com>
2023-02-22 23:04:35 -0500

    commit

---
+++ src/Button.js
@@ -0,0 +1,15 @@
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) {
+
+  return <button>{children}</button>;
+}
+
+export default Button;
