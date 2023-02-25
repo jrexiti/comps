dea35500cb5e79058fc3fe0e7c6e428e22145682
Jay Rexiti <jrexiti@totalwine.com>
2023-02-25 12:46:55 -0500

    logic for render dropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Dropdown from "./components/Dropdown";
+import { useState } from "react";
 
 function App() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
   const options = [
     { label: "red", value: "red" },
     { label: "green", value: "green" },
     { label: "blue", value: "blue" },
   ];
 
-  return <Dropdown options={options} />;
+  return (
+    <Dropdown options={options} selection={selection} onSelect={handleSelect} />
+  );
 }
 export default App;
