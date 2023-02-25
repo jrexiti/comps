f8d81decbec6c02c33e70d253f07a3fade3e2181
Jay Rexiti <jrexiti@totalwine.com>
2023-02-25 13:08:04 -0500

    added panel for styling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import Panel from "./Panel";
 
-function Dropdown({ options, selection, onSelect }) {
+function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -9,26 +11,33 @@ function Dropdown({ options, selection, onSelect }) {
 
   const handleOptionClick = (option) => {
     setIsOpen((currentIsOpen) => !currentIsOpen);
-    onSelect(option);
+    onChange(option);
   };
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
 
-  let content = "Select...";
-  if (selection) {
-    content = selection.label;
-  }
-
   return (
-    <div>
-      <div onClick={handleClick}>{content}</div>
-      {isOpen && <div>{renderedOptions}</div>}
+    <div className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </Panel>
+      {isOpen && (
+        <Panel className="absolute top-full border">{renderedOptions}</Panel>
+      )}
     </div>
   );
 }
