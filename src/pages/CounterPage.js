f7c79b1111ad1747476e2a028e93966388722250
Jay Rexiti <jrexiti@totalwine.com>
2023-02-27 17:15:08 -0500

    custom hook for counter

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,20 +1,14 @@
-import { useState, useEffect } from "react";
 import Button from "../components/Button";
+import useCounter from "../hooks/use-counter";
 
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
+  const { count, increment } = useCounter(initialCount);
   return (
     <div>
       <h1>Count is {count}</h1>
-      <Button onClick={handleClick}>Increment</Button>
+      <Button onClick={increment} primary>
+        Increment
+      </Button>
     </div>
   );
 }
