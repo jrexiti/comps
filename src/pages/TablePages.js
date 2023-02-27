e594f08f57b8e0e6629d0cbe8cf8b5a779ba5368
Jay Rexiti <jrexiti@totalwine.com>
2023-02-27 12:07:56 -0500

    sortable table comp setup

--- src/pages/TablePages.js
+++ src/pages/TablePages.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 function TablePage() {
   const data = [
@@ -14,7 +14,11 @@ function TablePage() {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      header: () => <th className="bg-red-500">Score</th>,
+    },
   ];
 
   const keyFn = (fruit) => {
@@ -22,7 +26,7 @@ function TablePage() {
   };
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   );
 }
