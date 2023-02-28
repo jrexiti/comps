03666d4965e39c6176a2f62e8886e259b5d6257e
Jay Rexiti <jrexiti@totalwine.com>
2023-02-28 16:52:41 -0500

    progress on counterPage

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -4,6 +4,7 @@ import Panel from "../components/Panel";
 
 function CounterPage({ initialCount }) {
   const [count, setCount] = useState(initialCount);
+  const [valueToAdd, setValueToAdd] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
@@ -13,6 +14,18 @@ function CounterPage({ initialCount }) {
     setCount(count - 1);
   };
 
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value) || 0;
+    setValueToAdd(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setCount(count + valueToAdd);
+
+    setValueToAdd(0);
+  };
+
   return (
     <Panel className="m-3">
       <h1 className="text-lg">Count is {count}</h1>
@@ -20,11 +33,13 @@ function CounterPage({ initialCount }) {
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Add a lot!</label>
         <input
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
+          value={valueToAdd || ""}
+          onChange={handleChange}
         />
         <Button>Add it</Button>
       </form>
