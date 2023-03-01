6aafd6e46c7953946a394a5f1c4605bc45d3a607
Jay Rexiti <jrexiti@totalwine.com>
2023-02-28 19:08:52 -0500

    implement action type

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,7 +2,24 @@ import Button from "../components/Button";
 import { useReducer } from "react";
 import Panel from "../components/Panel";
 
-const reducer = (state, action) => {};
+const INCREMENT_COUNT = "increment";
+const SET_VALUE_TO_ADD = "set-value-to-add";
+
+const reducer = (state, action) => {
+  if (action.type === INCREMENT_COUNT) {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+
+  if (action.type === SET_VALUE_TO_ADD)
+    return {
+      ...state,
+      valueToAdd: action.payload,
+    };
+  return state;
+};
 
 function CounterPage({ initialCount }) {
   // const [count, setCount] = useState(initialCount);
@@ -15,6 +32,8 @@ function CounterPage({ initialCount }) {
 
   const increment = () => {
     // setCount(count + 1);
+
+    dispatch({ type: INCREMENT_COUNT });
   };
 
   const decrement = () => {
@@ -24,6 +43,10 @@ function CounterPage({ initialCount }) {
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
     // setValueToAdd(value);
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
   };
 
   const handleSubmit = (event) => {
